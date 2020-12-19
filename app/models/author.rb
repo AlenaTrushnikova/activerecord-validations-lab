fe90f673bf6584b9ba0c5eb77ef12ffd64966dd3
Alena Trushnikova <alena.yu.t@gmail.com>
2020-12-19 11:15:20 -0800

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10}
+
+    # validates_presence_of :name
+    # validates_uniqueness_of :name
+    # validates_length_of :phone_number, is:10
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+    validates_presence_of :title
+    validates_length_of :content, minimum: 100
+    validates_length_of :summary, maximum: 100
+    validates :category, inclusion: {in:["Fiction", "Non-Fiction"]}
+
+    validate :clickbait
+
+    # def clickbait
+    #     if self.title
+    #       if !["Won't Believe", "Secret", "Top 3", "Guess"].any? {|str| self.title.include? str}
+    #       errors.add(:title, "Title is not clickbait!")
+    #       end
+    #     end
+    #   end
+
+
+    def clickbait
+        if self.title
+            if self.title[/Won't Believe|Secret|Top|Guess/] == nil
+                errors.add(:clickbait, "post is not clickabit")
+            end
+        end
+    end
 end
